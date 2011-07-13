a892ba9e26f6971ac8dcee1525757cb054bcae4e
wabu <waeber@inf.fu-berlin.de>
2011-07-13 18:29:14 +0200

    fixed makefiles for board/net

--- config.mk
+++ config.mk
@@ -10,17 +10,17 @@ LIBDIR = ${PREFIX}/lib
 INCDIR = ${PREFIX}/include
 
 # Includes and libs
-INCARGS += -I/usr/include -I/usr/local/include -I/usr/include/win32api
+INCARGS += -I/share/download/XMOS/DevelopmentTools/11.2.0/target/include -I/share/download/XMOS/DevelopmentTools/11.2.0/target/include/gcc
 LIBARGS += -L/usr/lib -L/usr/local/lib -lc -lGL -lGLU -lglut -lX11 -lm
 
 TARGET_BOARD = -target=XK-1
 # Flags
-CFLAGS += -Wall -g -O0 ${INCARGS}
+CFLAGS += -Wall $(TARGET_BOARD) -g -O0 ${INCARGS}
 LDFLAGS += -g ${LIBARGS}
 XCCFLAGS = -O3 -Wall $(TARGET_BOARD) ${INCARGS}
 
 # Compiler
-CC = cc -c
+CC = xcc -c
 XCC = xcc
 # Linker (Under normal circumstances, this should *not* be 'ld')
 LD = cc
@@ -29,11 +29,3 @@ SO = cc -shared
 # Archiver
 AR = ar crs
 
-# Tag files for vim
-TAGFILE=
-# you don't want to use ctags, comment out the next 2 lines
-TG = ctags
-TAGFILE=tags
-# you don't want to use scope, comment out the next 2 lines
-TG = echo | cscope $(INCARGS)
-TAGFILE=cscope.out
